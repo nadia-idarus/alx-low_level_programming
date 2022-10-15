f5950014399fdf74519512e555c26f67bcaebf4f
nadia-idarus <nadia.abdalla@riarauniversity.ac.ke>
2022-10-15 10:11:15 -0700

    2-alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,11 +3,11 @@
 #include <time.h>
 /**
  * main - Entry point
- * Return: Always 0
+ *  Return: Always 0
  */
 int main(void)
 {
-char low
+char low;
 for (low = 'a'; low <= 'z'; low++)
 putchar(low);
 putchar ('\n');
