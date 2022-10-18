c52d883d550ee62b3ffc15e40793a42394ce3079
nadia-idarus <nadia.abdalla@riarauniversity.ac.ke>
2022-10-18 10:08:40 -0700

    alphabet game

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
+#include <unistd.h>
 #include "main.h"
+
 /**
- * main - print _putchar
- * Return: 0
+ * main - check description
+ * Description:it prints the word _putchar, followed by a new line
+ * Return: Always 0
  */
 int main(void)
 {
-	_putchar('');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+
 	_putchar('\n');
 
 	return (0);
