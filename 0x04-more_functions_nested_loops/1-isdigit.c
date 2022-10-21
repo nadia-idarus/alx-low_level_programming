9c3bdf49a857fd1b90c1577503c17d778be11f02
nadia-idarus <nadia.abdalla@riarauniversity.ac.ke>
2022-10-21 10:41:02 -0700

    Digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checksfor a digit 0 through 9
+ * @c: carrier variable
+ * Return: 1 if true, 0 if false
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	else
+		return (0);
+}
