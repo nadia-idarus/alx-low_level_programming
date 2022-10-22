0c28ffff82aded4991e68e56d8392bac8e7538ea
nadia-idarus <nadia.abdalla@riarauniversity.ac.ke>
2022-10-22 08:59:09 -0700

    101-print numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_number - function that prints an integer
+ * @n: print int
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	/*first check if its negative*/
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	/*print the first few digits*/
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	/*print the last digit*/
+	_putchar((num % 10) + 48);
+}
