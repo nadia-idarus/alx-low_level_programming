8405510eafeeffdb522d55c49c4bd80ea804d650
nadia-idarus <nadia.abdalla@riarauniversity.ac.ke>
2022-11-01 06:20:59 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -1,22 +1,41 @@
 #include "main.h"
+
 #include <stdio.h>
+
+
 
 /**
  *
- * main - check the code
+ *  * main - check the code
  *
- * Return: Always 0.
+ *   *
  *
- */
-int main(void)
-{
-	char *s = "hello, world";
-       
-	char *f = "oleh"
-	     	unsigned int n;
+ *    * Return: Always 0.
+ *
+ *     */
 
-		        n = _strspn(s, f);
-			    printf("%u\n", n);
-			        return (0);
+int main(void)
+
+{
+
+	    char *s = "hello";
+
+	        char *f;
+
+
+
+		    f = _strchr(s, 'l');
+
+
+
+		        if (f != NULL)
+
+				    {
+
+					            printf("%s\n", f);
+
+						        }
+
+			    return (0);
 
 }
