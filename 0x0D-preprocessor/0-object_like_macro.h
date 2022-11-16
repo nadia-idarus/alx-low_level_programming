495334e39318ffa4ab3d3ac255df1e7387e49ced
nadia-idarus <nadia.abdalla@riarauniversity.ac.ke>
2022-11-16 05:54:23 -0800

    0-object

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+/**
+ * File: 0-object_like_macro.h
+ *  Description: A header filr that defines macro named
+ *  SIZE as an abbreviation for token 1024.
+ */
+#define SIZE 1024
+
+#endif
