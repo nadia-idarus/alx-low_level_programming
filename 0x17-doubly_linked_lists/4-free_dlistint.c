bf140c96063a412bc25a1a5f10aad8a3b594c46d
nadia-idarus <nadia.abdalla@riarauniversity.ac.ke>
2024-01-12 08:45:19 -0800

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h> // For free
+#include "your_header_file_containing_dlistint_t_definition.h"
+
+/**
+ * free_dlistint - Frees a dlistint_t list.
+ * @head: Pointer to the head of the doubly linked list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *temp;
+
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
